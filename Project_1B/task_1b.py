2c84a58c14b0af0bed904a686d5c61c7e2e19218
Linghao Zhang <linghao.zhang@protonmail.com>
2020-03-06 18:56:49 +0100

    Lasso optimization with K-Fold cross-validation

--- Project_1B/task_1b.py
+++ Project_1B/task_1b.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn import linear_model
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import KFold
 
 
 def read_data():
@@ -28,18 +29,64 @@ def get_features(x_data):
 
 def main():
 
+    # Constants
+    fold = 10
+    lambdas = np.logspace(-3, 3)
+
     # read data
     x_data, y_data = read_data()
 
     # Get all features
     x_data_all = get_features(x_data)
 
-    # Linear regression
-    reg = LinearRegression().fit(x_data_all, y_data)
+    # Empty loss function
+    R = pd.DataFrame(index=lambdas, columns=range(fold))
+
+    # Set of regression models
+    reg_list = []
+
+    # Define folds
+    kf = KFold(n_splits=fold)
+
+    # Repeat for each lambda
+    for lm in lambdas:
+
+        idx = 0
+
+        # Repeat for different test sets
+        for train_idx, test_idx in kf.split(x_data_all):
+
+            # Split into two groups
+            x_test = x_data_all[test_idx, :]
+            y_test = y_data[test_idx]
+            x_train = x_data_all[train_idx, :]
+            y_train = y_data[train_idx]
+
+            # Train and compute w
+            reg = linear_model.Lasso(lm)
+            reg.fit(x_train, y_train)
+
+            # Save coefficients
+            reg_list.append(reg.coef_)
+
+            # Prediction
+            y_pred = reg.predict(x_test)
+
+            # Loss
+            R[idx].loc[lm] = np.sqrt(mean_squared_error(y_test, y_pred))
+            idx += 1
+
+    # Find mean of each lambda
+    R_mean = R.mean(axis=1)
+
+    # Find minimum of loss
+    lm_min = R_mean.idxmin()
+    lm_min_idx = np.where(abs(lambdas - lm_min) <= 10 ** -3)[0][0]
+
+    print(lm_min_idx)
 
     # Write to file
-    np.savetxt('solution.csv', reg.coef_.transpose())
-    # reg.coef_.to_csv('solution.csv', index=False, header=False)
+    np.savetxt('solution.csv', reg_list[lm_min_idx].transpose())
 
 
 if __name__ == "__main__":
