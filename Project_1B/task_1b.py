5ed8fce342ab7992a3980f80f05e273139d2c633
Linghao Zhang <linghao.zhang@protonmail.com>
2020-03-09 21:12:02 +0100

    Built-in Lasso CV

--- Project_1B/task_1b.py
+++ Project_1B/task_1b.py
@@ -29,64 +29,19 @@ def get_features(x_data):
 
 def main():
 
-    # Constants
-    fold = 10
-    lambdas = np.logspace(-3, 3)
-
     # read data
     x_data, y_data = read_data()
 
     # Get all features
     x_data_all = get_features(x_data)
 
-    # Empty loss function
-    R = pd.DataFrame(index=lambdas, columns=range(fold))
-
-    # Set of regression models
-    reg_list = []
-
-    # Define folds
-    kf = KFold(n_splits=fold)
-
-    # Repeat for each lambda
-    for lm in lambdas:
-
-        idx = 0
-
-        # Repeat for different test sets
-        for train_idx, test_idx in kf.split(x_data_all):
-
-            # Split into two groups
-            x_test = x_data_all[test_idx, :]
-            y_test = y_data[test_idx]
-            x_train = x_data_all[train_idx, :]
-            y_train = y_data[train_idx]
-
-            # Train and compute w
-            reg = linear_model.Lasso(lm)
-            reg.fit(x_train, y_train)
-
-            # Save coefficients
-            reg_list.append(reg.coef_)
-
-            # Prediction
-            y_pred = reg.predict(x_test)
-
-            # Loss
-            R[idx].loc[lm] = np.sqrt(mean_squared_error(y_test, y_pred))
-            idx += 1
-
-    # Find mean of each lambda
-    R_mean = R.mean(axis=1)
-
-    # Find minimum of loss
-    lm_min = R_mean.idxmin()
-    lm_min_idx = np.where(abs(lambdas - lm_min) <= 10 ** -3)[0][0]
-
-    print(lm_min_idx)
+    # Ridge regression with built-in cv
+    reg = linear_model.LassoCV(
+        alphas=np.logspace(-3, 3, 121),
+        cv=10).fit(x_data_all, y_data)
 
     # Write to file
-    np.savetxt('solution.csv', reg_list[lm_min_idx].transpose())
+    np.savetxt('solution.csv', reg.coef_.transpose())
 
 
 if __name__ == "__main__":
