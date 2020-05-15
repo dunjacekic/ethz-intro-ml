2f1fb09e2336979cf7dd5c8d9f2b6b41d31b472c
yardenas <yardas@student.ethz.ch>
2020-05-15 22:56:09 +0200

    triplet loss yipikayayeey (?)

--- project_4/cnns4food.py
+++ project_4/cnns4food.py
@@ -4,8 +4,8 @@ import os
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
-IMG_WIDTH = 32
-IMG_HEIGHT = 32
+IMG_WIDTH = 48
+IMG_HEIGHT = 48
 
 
 def load_image(img):
@@ -18,10 +18,10 @@ def load_image(img):
 
 def load_triplets(triplet):
     ids = tf.strings.split(triplet)
-    a = load_image(tf.io.read_file('food/' + ids[0] + '.jpg'))
-    b = load_image(tf.io.read_file('food/' + ids[1] + '.jpg'))
-    c = load_image(tf.io.read_file('food/' + ids[2] + '.jpg'))
-    return tf.stack([a, b, c], axis=0), tf.strings.to_number(ids[3])
+    anchor = load_image(tf.io.read_file('food/' + ids[0] + '.jpg'))
+    truthy = load_image(tf.io.read_file('food/' + ids[1] + '.jpg'))
+    falsy = load_image(tf.io.read_file('food/' + ids[2] + '.jpg'))
+    return tf.stack([anchor, truthy, falsy], axis=0), [1, 1, 0]
 
 
 def create_model(freeze=True):
@@ -31,23 +31,18 @@ def create_model(freeze=True):
         include_top=False, input_shape=(IMG_HEIGHT, IMG_WIDTH, 3),
         weights=resnet_weights_path)
     encoder.trainable = not freeze
-    # encoder = tf.keras.models.Sequential()
-    # encoder.add(tf.keras.layers.Conv2D(32, (3, 3), padding='same', activation='relu'))
-    # encoder.add(tf.keras.layers.MaxPooling2D((2, 2)))
-    # encoder.add(tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu'))
-    # encoder.add(tf.keras.layers.MaxPooling2D((2, 2)))
-    # encoder.add(tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu'))
-    # encoder.add(tf.keras.layers.Flatten())
-    # tf.keras.layers.GlobalAveragePooling2D()
-    # encoder.add(tf.keras.layers.Dense(128, activation='relu'))
-    global_average = tf.keras.layers.GlobalAveragePooling2D()
-    image_a_features = global_average(encoder(inputs[:, 0, ...]))
-    image_b_features = global_average(encoder(inputs[:, 1, ...]))
-    image_c_features = global_average(encoder(inputs[:, 2, ...]))
-    distance_ab = tf.math.abs(image_a_features - image_b_features)
-    distance_ac = tf.math.abs(image_a_features - image_c_features)
-    final_logits = tf.keras.layers.Dense(1)(distance_ac - distance_ab)
-    triple_siamese = tf.keras.Model(inputs=inputs, outputs=final_logits)
+    encoder_layer = encoder.get_layer('conv3_block3_out')
+    decoder = tf.keras.Sequential([
+        tf.keras.layers.GlobalAveragePooling2D(),
+        tf.keras.layers.Dense(256, activation='relu')
+    ])
+    encoder_intermediate = tf.keras.Model(inputs=encoder.input, outputs=encoder_layer.output)
+    anchor, truthy, falsy = inputs[:, 0, ...], inputs[:, 1, ...], inputs[:, 2, ...]
+    anchor_features = decoder(encoder_intermediate(anchor))
+    truthy_features = decoder(encoder_intermediate(truthy))
+    falsy_features = decoder(encoder_intermediate(falsy))
+    embeddings = tf.stack([anchor_features, truthy_features, falsy_features], axis=-1)
+    triple_siamese = tf.keras.Model(inputs=inputs, outputs=embeddings)
     triple_siamese.summary()
     return triple_siamese
 
@@ -59,38 +54,21 @@ def make_training_labels():
     train_samples, val_samples = train_test_split(triplets, test_size=0.2)
     with open('val_samples.txt', 'w') as file:
         for item in val_samples:
-            flip = np.random.binomial(size=1, n=1, p=0.5)
-            a, b, c = item.rstrip().split()
-            sample = ' '.join([a, c, b, '0']) if flip else ' '.join([a, b, c, '1'])
-            file.write(sample + '\n')
+            file.write(item)
     with open('train_samples.txt', 'w') as file:
         for item in train_samples:
-            flip = np.random.binomial(size=1, n=1, p=0.5)
-            a, b, c = item.rstrip().split()
-            sample = ' '.join([a, c, b, '0']) if flip else ' '.join([a, b, c, '1'])
-            file.write(sample + '\n')
+            file.write(item)
     return len(train_samples)
 
 
-def show_batch(image_batch, label_batch):
-    plt.figure(figsize=(3, 6))
-    for i in range(4):
-        a, b, c = image_batch[i, ...]
-        ax = plt.subplot(4, 3, 3 * i + 1)
-        plt.imshow(a)
-        ax.get_xaxis().set_ticks([])
-        ax.get_yaxis().set_ticks([])
-        ax.set_ylabel(int(label_batch[i, ...]))
-        ax = plt.subplot(4, 3, 3 * i + 2)
-        plt.imshow(b)
-        ax.get_xaxis().set_ticks([])
-        ax.get_yaxis().set_ticks([])
-        ax = plt.subplot(4, 3, 3 * i + 3)
-        plt.imshow(c)
-        ax.get_xaxis().set_ticks([])
-        ax.get_yaxis().set_ticks([])
-    plt.tight_layout()
-    plt.show()
+def triplet_loss(_, embeddings):
+    anchor, truthy, falsy = embeddings[..., 0], embeddings[..., 1], embeddings[..., 2]
+    distance_truthy = tf.reduce_sum(tf.square(anchor - truthy), 1)
+    distance_falsy = tf.reduce_sum(tf.square(anchor - falsy), 1)
+    return tf.reduce_mean(tf.maximum(distance_truthy - distance_falsy + 1.0, 0.0))
+
+def infer(embeddings):
+    anchor, truthy, falsy = embeddings[..., 0], embeddings[..., 1], embeddings[..., 2]
 
 
 def main():
@@ -113,18 +91,16 @@ def main():
         load_triplets,
         num_parallel_calls=tf.data.experimental.AUTOTUNE)
     model = create_model()
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
-                  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.00001),
+                  loss=triplet_loss)
     train_dataset = train_dataset.shuffle(1024).batch(args.batch_size).repeat()
     val_dataset = val_dataset.batch(args.batch_size)
-    image, label = next(iter(train_dataset))
-    show_batch(image.numpy(), label.numpy())
     history = model.fit(
         train_dataset,
         steps_per_epoch=num_train_samples // args.batch_size,
         epochs=args.epochs,
-        validation_data=val_dataset
+        # validation_data=val_dataset,
+        # validation_steps=50
     )
 
 
